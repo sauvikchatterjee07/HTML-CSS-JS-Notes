47cb6e5d2b5b9982348e6c6861b5127700097dec
unknown <sauvikchatterjee9@gmail.com>
2023-10-26 20:15:50 +0530

    class 3 added

---
+++ JS-3/class1.js
@@ -0,0 +1,85 @@
+// var age = 18;
+// var checkAge;
+
+// age >= 18 ? (checkAge = "You are allowed") : (checkAge = "Not Allowed");
+
+// console.log(checkAge);
+
+// var age = 18;
+// var checkAge;
+
+// if (!"0") {
+//   checkAge = "You are allowed";
+// } else {
+//   checkAge = "Not Allowed";
+// }
+
+// console.log(checkAge);
+
+// Truthy Values => true, {}, [], any value (6237, 745, 25, 21..), "0", "false", Infinity, -Infinity
+
+// Falsy Value => false, undefined, null, NaN, "", 0, -0
+
+//if-else => if, else, else if
+//Switch Case
+
+// switch(expression){
+//     case value1:
+//         //ewdjkfoi2rh
+//         break;
+
+//     case value2:
+//         //jheqdgfuyiwbg
+//         break;
+// }
+
+// for (var i = 0; i < 10; i++) {
+//   console.log("hello");
+// }
+
+// for in => Object
+// for of loop => Array & String
+
+// var arr1 = ["wjfder", 10, true, [12, 67.78, "jufercbui"]];
+// var arr1 = ["wjfder", 10, true];
+
+// for (var i = 0; i < arr1.length; i++) {
+//   console.log(arr1[i]);
+// }
+
+// for (ele of arr1) {
+//   console.log(element);
+// }
+
+// Form
+// username - => String
+// password - => String
+
+// [string, string, string]
+
+// var arr = ["1234", "Sauvik", 20, false, ["red", "yellow", "blue"]];
+
+// var obj = {
+//   username: "Sauvik",
+//   password: "1234",
+//   age: 20,
+//   isVegetarian: false,
+//   favouriteColors: ["red", "yellow", "blue"],
+//   education: {
+//     college: "BIMS",
+//     courseFee: 24000,
+//   }
+// };
+
+//While loop
+//do-while loop
+
+// console.log(obj.username);
+// console.log(obj.favouriteColors[2]);
+
+// console.log(obj.education.courseFee);
+// console.log(obj["education"]["courseFee"]);
+
+// console.log(obj["password"]);
+
+//retrieving => 1.dot notation & 2.[] notation
